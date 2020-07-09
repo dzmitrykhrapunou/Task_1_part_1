656d4c93cb9b21c6ffd7b9fc2b1b704f9d264d6d
Dzmitry_Khrapunou <dmitry_victory@inbox.ru>
2020-07-09 22:03:59 +0300

    Init commit

---
+++ Task_1_part_1_Dzmitry_Khrapunou/Task_1_part_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_1_part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа");
+            Console.Write("A = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Наибольший общий делитель чисел {0} и {1} равен {2}", a, b, Class1.NOD(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}
