262a18f2057ad21f244399bcd275d54c9305e8e3
Dzmitry_Khrapunou <dmitry_victory@inbox.ru>
2020-07-10 19:27:06 +0300

    no message

---
+++ Task_1_part_1_Dzmitry_Khrapunou/Task_1_part_1/AlgorithmSCD.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1_part_1
+{
+    public class AlgorithmGCD
+    {
+        public static int GCD(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+            return b;
+        }
+        public static int GCD(int a, int b, int c)
+        {
+            int n = GCD(a, b);
+            while (n != c)
+            {
+                if (n > c)
+                {
+                    n = n - c;
+                }
+                else
+                {
+                    c = c - n;
+                }
+            }
+            return c;
+        }
+        public static int GCD(int a, int b, int c, int d)
+        {
+            int n = GCD(a, b);
+            int m = GCD(c, d);
+            while (n != m)
+            {
+                if (n > m)
+                {
+                    n = n - m;
+                }
+                else
+                {
+                    m = m - n;
+                }
+            }
+            return m;
+        }
+        public static int GCD(int a, int b, int c, int d, int e)
+        {
+            int n = GCD(a, b);
+            int m = GCD(c, d, e);
+            while (n != m)
+            {
+                    if (n > m)
+                    {
+                        n = n - m;
+                    }
+                    else
+                    {
+                        m = m - n;
+                    }
+            }
+            return m;
+        }
+    }
+}
--- Task_1_part_1_Dzmitry_Khrapunou/Task_1_part_1/Program.cs
+++ Task_1_part_1_Dzmitry_Khrapunou/Task_1_part_1/Program.cs
@@ -6,12 +6,22 @@ namespace Task_1_part_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите два числа");
+            Console.WriteLine("Алгоритм Евклида");
+            Console.WriteLine("Введите пять чисел");
             Console.Write("A = ");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.Write("B = ");
             int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Наибольший общий делитель чисел {0} и {1} равен {2}", a, b, Class1.NOD(a, b));
+            Console.Write("C = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("D = ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.Write("E = ");
+            int e = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Наибольший общий делитель для первых двух чисел {0}, {1} равен {2}", a, b, AlgorithmGCD.GCD(a, b));
+            Console.WriteLine("Наибольший общий делитель для первых трех чисел {0}, {1}, {2} равен {3}", a, b, c, AlgorithmGCD.GCD(a, b, c));
+            Console.WriteLine("Наибольший общий делитель для первых четырех чисел {0}, {1}, {2}, {3} равен {4}", a, b, c, d, AlgorithmGCD.GCD(a, b, c, d));
+            Console.WriteLine("Наибольший общий делитель для пяти чисел {0}, {1}, {2}, {3}, {4} равен {5}", a, b, c, d, e, AlgorithmGCD.GCD(a, b, c, d, e));
 
             Console.ReadKey();
         }
