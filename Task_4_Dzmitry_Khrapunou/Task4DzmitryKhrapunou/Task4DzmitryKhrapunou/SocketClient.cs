e3d9ed4f4b32fed4a29f75711fd9e0e9dae265ec
Dzmitry_Khrapunou <dmitry_victory@inbox.ru>
2020-08-03 19:20:54 +0300

    the server and client were created

---
+++ Task_4_Dzmitry_Khrapunou/Task4DzmitryKhrapunou/Task4DzmitryKhrapunou/SocketClient.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Task4DzmitryKhrapunouSoketClient
+{
+    public class SocketClient
+    {
+        /// <summary>
+        /// Local endpoint
+        /// </summary>
+        IPEndPoint ipEndPoint = null;
+
+        /// <summary>
+        /// Server's IP
+        /// </summary>
+        IPAddress ipAddr = null;
+
+        /// <summary>
+        /// Socket instance
+        /// </summary>
+        Socket sender;
+
+        /// <summary>
+        /// Server IP
+        /// </summary>
+        public string ServerIP { get; private set; }
+
+        /// <summary>
+        /// Port number
+        /// </summary>
+        public int Port { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the ClientSocket class
+        /// </summary>
+        /// <param name="client"> Client instance</param>
+        /// <param name="serverIP"> Server IP</param>
+        /// <param name="port"> Port number</param>
+        public SocketClient(int port = 1408, string serverIP = "127.0.0.1")
+        {
+            ServerIP = serverIP;
+            Port = port;
+        }
+
+        public void Connect()
+        {
+            ipAddr = IPAddress.Parse(ServerIP);
+            ipEndPoint = new IPEndPoint(ipAddr, Port);
+
+            sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(ipEndPoint);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }           
+        }
+
+        /// <summary>
+        /// Delegate accepting any method 'void(string)
+        /// </summary>
+        /// <param name="msg"></param>
+        public delegate void MessageFromServer(string msg);
+        /// <summary>
+        /// Event to get message from server
+        /// </summary>
+        public event MessageFromServer GetMessageFromServer;
+
+        /// <summary>
+        /// Get message from server
+        /// </summary>
+        /// <param name="msg"></param>
+        public string GetMsg()
+        {
+            byte[] receivedBytes = new byte[1024];
+            var size = 0;
+            var serverAnswer = new StringBuilder();
+
+            do
+            {
+                size = sender.Receive(receivedBytes);
+                serverAnswer.Append(Encoding.UTF8.GetString(receivedBytes, 0, size));
+            } 
+            while (sender.Available > 0);
+
+            GetMessageFromServer?.Invoke(serverAnswer.ToString());
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+            return serverAnswer.ToString();
+        }
+
+        /// <summary>
+        /// Send messeage to server
+        /// </summary>
+        /// <param name="msg"></param>
+        public void SendMsg(string msg)
+        {
+            var data = Encoding.UTF8.GetBytes(msg);
+            sender.Connect(ipEndPoint);
+            sender.Send(data);
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+    }
+}
+
---
+++ Task_4_Dzmitry_Khrapunou/Task4DzmitryKhrapunou/Task4DzmitryKhrapunou/SocketServer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Task4DzmitryKhrapunouSocketServer
+{
+    public class SocketServer
+    {
+        /// <summary>
+        /// Local endpoint
+        /// </summary>
+        IPEndPoint ipEndPoint = null;
+
+        /// <summary>
+        /// Server socket instance
+        /// </summary>
+        Socket serverSocket = null;
+
+        /// <summary>
+        /// Server listener instance
+        /// </summary>
+        Socket serverListener = null;
+
+        /// <summary>
+        /// Port number
+        /// </summary>
+        public int Port { get; private set; }
+
+        /// <summary>
+        /// Server IP address.
+        /// </summary>
+        public string IpAdress { get; private set; }
+              
+        public SocketServer(int port = 1408, string ipAdress = "127.0.0.1")
+        {
+            Port = port;
+            IpAdress = ipAdress;
+
+            ipEndPoint = new IPEndPoint(IPAddress.Parse(ipAdress), port);
+            serverListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverListener.Bind(ipEndPoint);
+        }
+
+        /// <summary>
+        /// Method waiting and get message from clients
+        /// </summary>
+        /// <param name="num"></param>
+        public void ListenClient(int num)
+        {
+            if (serverListener != null)
+            {
+                serverListener.Listen(num);
+                serverSocket = serverListener.Accept();
+            }
+            else
+            {
+                throw new Exception("The server listener was not created");
+            }
+
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+
+                var size = 0;
+
+                StringBuilder data = new StringBuilder();
+
+                do
+                {
+                    size = serverListener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (serverListener.Available > 0);
+
+                byte[] bytes = Encoding.UTF8.GetBytes("The message is successfully received");
+                serverSocket.Send(bytes);
+
+                serverListener.Shutdown(SocketShutdown.Both);
+                serverListener.Close();
+            }
+        }
+    }
+}
