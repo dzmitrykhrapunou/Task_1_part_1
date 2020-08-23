7569646d393d0b05bb8a0378bb8659f92a7c7271
Dzmitry_Khrapunou <dmitry_victory@inbox.ru>
2020-08-23 14:49:26 +0300

    no message

---
+++ Task_5_Dzmitry_Khrapunou/Task5DzmitryKhrapunou/Task5Part2DzmitryKhrapunou/Serializer.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+
+namespace Task5Part2DzmitryKhrapunou
+{
+    public static class Serializer<T>
+    {
+        /// <summary>
+        /// Writes data to the binary file.
+        /// </summary>
+        /// <param name="filePath">Path to file</param>
+        /// <param name="obj">Data to write</param>
+        public static void SerializeToBinary(string filePath, T obj)
+        {
+            File.WriteAllText(filePath, string.Empty);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(fileStream, obj);
+            }
+        }
+
+        /// <summary>
+        /// Writes data to the json file.
+        /// </summary>
+        /// <param name="filePath">Path to file</param>
+        /// <param name="obj">Data to write</param>
+        public static void SerializeToJson(string filePath, T obj)
+        {
+            File.WriteAllText(filePath, string.Empty);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var jsonFormatter = new DataContractJsonSerializer(typeof(T));
+                jsonFormatter.WriteObject(fileStream, obj);
+            }
+        }
+
+        /// <summary>
+        /// Writes data to the xml file.
+        /// </summary>
+        /// <param name="filePath">Path to file</param>
+        /// <param name="obj">Data to write</param>
+        public static void SerializeToXml(string filePath, T obj)
+        {
+            File.WriteAllText(filePath, string.Empty);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var xmlFormatter = new DataContractSerializer(typeof(T));
+                xmlFormatter.WriteObject(fileStream, obj);
+            }
+        }
+
+        /// <summary>
+        /// Reads data from the binary file.
+        /// </summary>
+        /// <param name="filePath">Path to file</param>
+        /// <returns>Deserialized object</returns>
+        public static T DeserializeFromBinary(string filePath)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(fileStream);
+            }
+        }
+
+        /// <summary>
+        /// Reads data from the json file.
+        /// </summary>
+        /// <param name="filePath">Path to file</param>
+        /// <returns>Deserialized object</returns>
+        public static T DeserializeFromJson(string filePath)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var jsonFormatter = new DataContractJsonSerializer(typeof(T));
+                return (T)jsonFormatter.ReadObject(fileStream);
+            }
+        }
+
+        /// <summary>
+        /// Reads data from the xml file.
+        /// </summary>
+        /// <param name="filePath">Path to file</param>
+        /// <returns>Deserialized object</returns>
+        public static T DeserializeFromXml(string filePath)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var xmlFormatter = new DataContractSerializer(typeof(T));
+                return (T)xmlFormatter.ReadObject(fileStream);
+            }
+        }
+
+    }
+}
